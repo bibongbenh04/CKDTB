b75594df0ed4b73bf9fafa94dffb2cf4e44e6e29
Phạm Đức Quý <107932478+DucQuy2032002@users.noreply.github.com>
2023-08-17 01:08:35 +0700

    Update function.sql

--- function.sql
+++ function.sql
@@ -2,32 +2,30 @@
 --function 1 : Viết hàm in ra danh sách giáo viên bao gồm mã gv, tên gv, giới tính, ngày sinh, địa chỉ, sdt, môn học mà giáo viên đó dạy.
 --Input :
 --Output :
-Create function fuInDanhSachGV
-(
-)
+Create function fuInDanhSachGV()
 returns @DSGV table (magv char(10), tengv nvarchar(30), gioitinh char(3), ngaysinh datetime, 
-					diachi varchar(150), sdt varchar(20), monhocdamnhiem nvarchar(30))
+					DIACHI varchar(150), sdt varchar(20), monhocdamnhiem nvarchar(30))
 as
 begin
-	insert into @DSGV (magv, tengv, gioitinh, ngaysinh, diachi, sdt, monhocdamnhiem)
+	insert into @DSGV (magv, tengv, gioitinh, ngaysinh, DIACHI, sdt, monhocdamnhiem)
 	select MAGV, HOTEN, GIOITINH, NGAYSINH, DIACHI, DIENTHOAI, TENMH
 	from GIAOVIEN GV, MONHOC MH
-	where GV.MAMH = MH.MAMH and GV.MAMH = MH.TENMH
+	where GV.MAMH = MH.MAMH
 
 	return;
 end
-
+SELECT *FROM GIAOVIEN
 go
 --gọi thực thi
-select*from dbo.fuInDanhSachGV ()
+select*from fuInDanhSachGV ()
 
 --function 2: Tạo function cho biết số lượng học sinh của một lớp với mã lớp là tham số truyền vào
-Create function fuDemSoLuongHS (@malop char(10))
-returns int
-as
+CREATE function fuDemSoLuongHS (@malop char(10))
+returns int 
+as 
 begin
 	declare @dem int;
-	select @dem = count(HS.MALOP)
+	select @dem = count(HS.MALOP) 
 	from HOCSINH HS
 	where HS.MALOP = @malop
 	return @dem;
@@ -38,6 +36,7 @@ go
 select dbo.fuDemSoLuongHS ('L01')
 
 select dbo.fuDemSoLuongHS ('L02')
+
 --function 3 : Viết hàm in ra danh sách học sinh bao gồm mã học sinh, tên học sinh thuộc lớp đó với mã lớp là tham số truyền vào
 Create function fuDanhSachHS (@malop char(10))
 returns table 
@@ -51,11 +50,11 @@ return
 
 --gọi thực thi
 go 
-select dbo.fuDemSoLuongHS ('L01')
+select * from dbo.fuDanhSachHS ('L01')
 
-select dbo.fuDemSoLuongHS ('L02')
+select * from dbo.fuDanhSachHS ('L02')
 
--- function 4 : Viết hàm in điểm thi của học sinh bao gồm tên môn học, học kì, điểm thi miệng, điểm thi 15p, điểm thi 45p
+--function 4 : Viết hàm in điểm thi của học sinh bao gồm tên môn học, học kì, điểm thi miệng, điểm thi 15p, điểm thi 45p
 --điểm thi học kì với mã học sinh là tham số truyền vào
 Create function fuInDiemThiHS (@mahs char(10))
 returns @DTHS table (tenmonhoc nvarchar(30),hocki char(10), diemmieng float, diem15p float, diem45p float, diemhocki float)
@@ -64,12 +63,11 @@ begin
 	insert into @DTHS (tenmonhoc, hocki, diemmieng, diem15p, diem45p, diemhocki)
 	select TENMH, HOCKY, DIEMTHIMIENG, DIEMTHI15P, DIEMTHI1TIET, DIEMTHIHOCKY
 	from KETQUA KQ, MONHOC MH
-	where KQ.MAMH = MH.MAMH and KQ.MAMH = MH.TENMH
-
+	where KQ.MAMH = MH.MAMH and KQ.MAHS = @mahs
 	return;
 end
 
 --gọi thực thi
 select*from dbo.fuInDiemThiHS ('10HS00001')
 
-select*from dbo.fuInDiemThiHS ('10HS00002')
+select*from dbo.fuInDiemThiHS ('10HS00003')
